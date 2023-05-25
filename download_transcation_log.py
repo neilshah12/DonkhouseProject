40dae057823f14d6c8d3027661a183ea2c4d8c6d
BrandonDu <bdu8281@gmail.com>
2023-05-25 17:11:25 -0400

    cleaned up code

--- download_transcation_log.py
+++ download_transcation_log.py
@@ -1,6 +1,4 @@
-import re
-import time
-import os
+import re, time, os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -13,7 +11,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 current_dir= os.getcwd()
 download_dir = os.path.join(current_dir, 'logs')
 prefs = {'download.default_directory': download_dir}
-
 
 chrome_options = Options()
 chrome_options.add_experimental_option('prefs', prefs)
@@ -42,26 +39,19 @@ canvas_locator = (By.TAG_NAME, 'canvas')
 wait.until(EC.presence_of_element_located(canvas_locator))
 
 canvas = driver.find_element(By.TAG_NAME, 'canvas')
-canvas_x = canvas.location['x']
-canvas_y = canvas.location['y']
-print(canvas_x)
-print(canvas_y)
 style = canvas.get_attribute('style')
-width = re.search(r"width:\s*([\d.]+)px", style).group(1)
-height = re.search(r"height:\s*([\d.]+)px", style).group(1)
-
-print(float(width))
-print(float(height))
+canvas_width = float(re.search(r"width:\s*([\d.]+)px", style).group(1))
+canvas_height = float(re.search(r"height:\s*([\d.]+)px", style).group(1))
 
 time.sleep(5)
 # Calculate button position within the canvas
-button_x = 178/847
-button_y = 40/529.375
+DOWNLOAD_X_RATIO = 178/847
+DOWNLOAD_Y_RATIO = 40/529.375
+download_x = round((DOWNLOAD_X_RATIO - 0.5) * canvas_width)
+download_y = round((DOWNLOAD_Y_RATIO - 0.5) * canvas_height)
 
 action_chains = ActionChains(driver)
-action_chains.move_to_element_with_offset(canvas, button_x * float(width) - (float(width)//2), button_y * float(height) - (float(height)//2)).click().perform()
-
-time.sleep(3)
+action_chains.move_to_element_with_offset(canvas, download_x, download_y).click().perform()
+time.sleep(1)
 action_chains.move_to_element_with_offset(canvas, 0, 0).click().perform()
-
-time.sleep(10)
+time.sleep(5)
