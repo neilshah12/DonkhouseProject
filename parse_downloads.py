f556b3883a7aba2d403479361ab67997897b4e82
BrandonDu <82884213+BrandonDu@users.noreply.github.com>
2023-05-27 14:10:15 -0400

    stats finished

--- parse_downloads.py
+++ parse_downloads.py
@@ -1,40 +1,43 @@
 from datetime import datetime as dt
 from typing import Dict
+from player import Player
 
 import math
 import pandas as pd
 import pickle
 import re
 
-from player import Player
 
 try:
     with open('last_time.pickle', 'rb') as file:
-        latest_time = pickle.load(file)
+        prev_latest_time = pickle.load(file)
 except (EOFError, FileNotFoundError):
-    latest_time = dt.min
+    prev_latest_time = dt.min
 
-new_latest_time = latest_time
-all_players = Dict[str, Player]
+# curr_latest_time = prev_latest_time
+all_players: Dict[str, Player] = {}
 
 
 def update_latest_time():
     with open('last_time.pickle', 'wb') as f:
-        pickle.dump(latest_time, f)
+        pickle.dump(prev_latest_time, f)
 
 
 def update_players(players_dict, game_players):
-    for player in game_players:
-        if player in players_dict:
+    for key in game_players:
+        player = game_players[key]
+        if player.username in players_dict:
             dict_player = players_dict.get(player.username)
             dict_player.net += player.net
-            dict_player.hands += player.hands_seen
+            dict_player.hands_seen += player.hands_seen
+            dict_player.hands_played += player.hands_played
+            dict_player.hands_raised_pre += player.hands_raised_pre
         else:
-            players_dict[player.username] = player
+            players_dict[key] = player
 
 
 def parse_nets(in_and_outs: str):
-    global new_latest_time
+    global curr_latest_time
     df = pd.read_csv(in_and_outs, skiprows=1, skip_blank_lines=False)
 
     game_players = list()
@@ -45,78 +48,72 @@ def parse_nets(in_and_outs: str):
             game_players.clear()
         elif user == 'End time:' and math.isnan(net):
             curr_end_time = dt.strptime(row['In'], '%Y-%m-%d %H:%M:%S')
-            new_latest_time = max(new_latest_time, curr_end_time)
-            if curr_end_time <= latest_time:
+            if curr_end_time <= prev_latest_time:
                 return
+            curr_latest_time = max(curr_latest_time, curr_end_time)
             update_players(all_players, game_players)
         elif not math.isnan(net):
             game_players.append(Player(username=user, net=net))
 
 
 def parse_stats(hand_histories: str):
-    count = 0
+    new_game_pattern = r'(\ufeff)?\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}: New hand \(ID [a-zA-Z0-9]+\) of NL Texas Holdem'
+    time_pattern = r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})'
+    bb_player_pattern = r'^([a-zA-Z0-9_.-]+) \(\d+(\.\d{1,2})?, BB\)'
+    in_hand_pattern = r'^[a-zA-Z0-9_.-]+ \(\d+(\.\d{1,2})?, [A-Z0-9+]+\)'
+    bb_post_pattern = r'^[a-zA-Z0-9_.-]+ posted (\d+(\.\d{1,2})?)'
+    flop_pattern = r'^board: ([2-9]|10|J|Q|K|A)\? ([2-9]|10|J|Q|K|A)\? ([2-9]|10|J|Q|K|A)\? '
+    won_pattern = r'^([a-zA-Z0-9_.-]+) won \d+(\.\d{1,2})? chips'
+    vpip_pattern = r'^([a-zA-Z0-9_.-]+) (called \d+(\.\d{1,2})?|raised to \d+(\.\d{1,2})?)'
+    raise_pattern = r'^[a-zA-Z0-9_.-]+ raised to \d+(\.\d{1,2})?'
+
     with open(hand_histories, 'r') as f:
-        new_game_pattern = r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}: New hand \(ID [a-zA-Z0-9]+\) of NL Texas Holdem$'
-        time_pattern = r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'
-        bb_player_pattern = r'^([a-zA-Z0-9_.-]+) \(\d+(\.\d{1,2})?, BB\)'
-        in_hand_pattern = r'^[a-zA-Z0-9_.-]+ \(\d+(\.\d{1,2})?, [A-Z]{2}\)'
-        bb_post_pattern = r'^[a-zA-Z0-9_.-]+ posted (\d+(\.\d{1,2})?)$'
-        flop_pattern = r'^board: [2-10JKQA][♣♦♥♠] [2-10JKQA][♣♦♥♠] [2-10JKQA][♣♦♥♠] $'
-        turn_pattern = r'^board: [2-10JKQA][♣♦♥♠] [2-10JKQA][♣♦♥♠] [2-10JKQA][♣♦♥♠] [2-10JKQA][♣♦♥♠] $'
-        river_pattern = r'^board: [2-10JKQA][♣♦♥♠] [2-10JKQA][♣♦♥♠] [2-10JKQA][♣♦♥♠] [2-10JKQA][♣♦♥♠] [2-10JKQA][♣♦♥♠] $'
-        won_pattern = r'^([a-zA-Z0-9_.-]+) won (\d+(\.\d{1,2}) chips'
-        vpip_pattern = r'^([a-zA-Z0-9_.-]+) (called \d+(\.\d{1,2})?|raised to \d+(\.\d{1,2})?)$'
-        player_dict = Dict[str, Player]
+        player_dict: Dict[str, Player] = {}
         for line in f:
-            game_over = False
-            # hand started
-            if re.match(pattern=new_game_pattern, string=line):
-                time = dt.strptime(re.match(pattern=time_pattern, string=line).group(1), '%Y-%m-%d %H:%M:%S')
-                if time <= latest_time:
-                    continue
-                game_players = Dict[str, Player]
-                line = f.readline()
-                bb_player = str
-                while not re.match(pattern=bb_post_pattern, string=line):
-                    if re.match(pattern=in_hand_pattern, string=line):
-                        player_name = re.match(r'^([a-zA-Z0-9_.-]+)').group(1)
-                        game_players[player_name] = Player(username=player_name, hands_seen=1)
-                        if re.match(pattern=bb_player_pattern, string=line):
-                            bb_player = player_name
-                    line = f.readline()
+            if re.match(pattern=new_game_pattern, string=line) is None:
+                continue
+            time = dt.strptime(re.search(pattern=time_pattern, string=line).group(1), '%Y-%m-%d %H:%M:%S')
 
-                is_walk = True
+            if time <= prev_latest_time:
+                continue
+            elif time > curr_latest_time:
+                update_players(all_players, player_dict)
+                return
+
+            game_players: Dict[str, Player] = {}
+            bb_player = str
+            line = f.readline()
+
+            while re.match(pattern=bb_post_pattern, string=line) is None:
+                if re.match(pattern=in_hand_pattern, string=line):
+
+                    player_name = re.match(pattern=r'^([a-zA-Z0-9_.-]+)', string=line).group(1)
+                    game_players[player_name] = Player(username=player_name, hands_seen=1)
+                    if re.match(pattern=bb_player_pattern, string=line):
+                        bb_player = player_name
                 line = f.readline()
-                while not re.match(pattern=flop_pattern, string=line):
-                    if re.match(pattern=won_pattern, string=line):
-                        game_over = True
-                        if is_walk:
-                            del game_players[bb_player]
-                        break
+
+            is_walk = True
+            line = f.readline()
+            while re.match(pattern=flop_pattern, string=line) is None:
+                if re.match(pattern=won_pattern, string=line):
+                    if is_walk:
+                        del game_players[bb_player]
+                    break
+                if re.match(pattern=vpip_pattern, string=line) is not None:
                     player = re.match(pattern=vpip_pattern, string=line).group(1)
-                    if player is not None:
-                        game_players.get(player).hands_played = 1
-                        is_walk = False
-
-                if not game_over:
-                    line = f.readline()
-                    while not re.match(pattern=won_pattern, string=line):
-                        line = f.readline()
-
-                    winner = re.match(pattern=won_pattern, string=line).group(1)
-                    while winner is not None:
-                        game_players.get(winner)
-
-                update_players(player_dict, game_players)
+                    is_walk = False
+                    game_players[player].hands_played = 1
+                    if re.match(pattern=raise_pattern, string=line) is not None:
+                        game_players[player].hands_raised_pre = 1
+                line = f.readline()
+            update_players(player_dict, game_players)
+            game_players.clear()
+        update_players(all_players, player_dict)
 
 
-#
-# parse_nets(r"/Users/brandondu/Downloads/Test.csv")
-# for key, value in all_players.items():
-#     print(f"{key}: {value}")
-
-# parse_stats(r'/Users/brandondu/Downloads/game_log_2023-5-25_20-12EST.txt')
-
-# preflop_action_pattern = r'^([a-zA-Z0-9_.-]+) (called \d+(\.\d{1,2})?|raised to \d+(\.\d{1,2})?|folded)$'
-# text = "Player1 folded"
-# print(re.match(preflop_action_pattern, text).group(1))
+prev_latest_time = dt.min
+curr_latest_time = dt.strptime('2023-05-25 20:12:37', '%Y-%m-%d %H:%M:%S')
+parse_stats(r'/Users/brandondu/Downloads/Test.txt')
+for p in all_players:
+    print(all_players[p])
--- player.py
+++ player.py
@@ -1,9 +1,11 @@
 class Player:
-    def __init__(self, username: str, net: float = 0, hands_seen: int = 0, hand_played: int = 0):
+    def __init__(self, username: str, net: float = 0, hands_seen: int = 0, hands_played: int = 0,
+                 hands_raised_pre: int = 0):
         self.username = username
         self.net = net
-        self.hands = hands_seen
-        self.hands = hand_played
+        self.hands_seen = hands_seen
+        self.hands_played = hands_played
+        self.hands_raised_pre = hands_raised_pre
 
     def __eq__(self, other):
         if isinstance(other, Player):
@@ -14,4 +16,6 @@ class Player:
         return hash(self.username)
 
     def __str__(self):
-        return f"Player(username={self.username}, net={self.net}, hands_seen={self.hands_seen})"
+        return f"Player(username={self.username}, net={self.net}, hands_seen={self.hands_seen}, " \
+               f"hands_played={self.hands_played}, hands_raised_pre={self.hands_raised_pre})"
+
