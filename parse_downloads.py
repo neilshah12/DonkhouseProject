538b0b916b4b27a901fb2121474e7bd49fba1d0a
neilshah12 <90114248+neilshah12@users.noreply.github.com>
2023-06-21 14:31:28 -0400

    Update parse_downloads.py

--- parse_downloads.py
+++ parse_downloads.py
@@ -262,7 +262,6 @@ def parse_stats(hand_histories, prev_info, curr_info):  # hand histories
 
 
 def main():
-    init_info()
     prev_info = load_info()
     curr_info = prev_info.copy()
     parse_nets(sys.argv[2], curr_info)
