b99dde7bd431068dd2873d93f5c33adfaeb3befb
neilshah12 <n.shah@yale.edu>
2023-07-09 19:50:55 -0400

    pagination functionality and some styling

--- client/src/PlayerInfo.js
+++ client/src/PlayerInfo.js
@@ -1,44 +1,110 @@
-import { Link, useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from 'react-router-dom';
+import React, { useState } from 'react';
 
 export default function PlayerInfo() {
-    const { username } = useParams()
-    const player_info = useLoaderData()
-    const statistics = JSON.parse(player_info[0].stats);
-    const statistics_entries = Object.entries(statistics).filter(([key]) => !["username", "net", "raised"].includes(key));
-    return (
-        <div className="playerinfo">
+  const { username } = useParams();
+  const player_info = useLoaderData();
+  const [leaderboardOption, setLeaderboardOption] = useState('games');
+  const statistics = JSON.parse(player_info[0].stats);
+  const statistics_entries = Object.entries(statistics).filter(
+    ([key]) => !['username', 'net', 'raised'].includes(key)
+  );
+  const [currentPage, setCurrentPage] = useState(1);
+  const [gamesPerPage] = useState(10);
+
+  const handleLeaderboardToggle = (option) => {
+    setLeaderboardOption(option);
+    setCurrentPage(1); // Reset to the first page when toggling options
+  };
+
+  // Get current games
+  const indexOfLastGame = currentPage * gamesPerPage;
+  const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+  const currentGames = player_info.slice(indexOfFirstGame, indexOfLastGame);
+
+  // Change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  return (
+    <div className="playerinfo">
+      <div className="leaderboard-table">
+        <div className="flex">
+          <h3>{username}</h3>
+          <div className="item">
+            {(JSON.parse(player_info[0].stats).net >= 0 ? '+$' : '-$') +
+              Math.abs(JSON.parse(player_info[0].stats).net)}
+          </div>
+        </div>
+      </div>
+
+      <div className="board">
+        <div className="duration">
+          <button
+            className={leaderboardOption === 'games' ? 'active' : ''}
+            onClick={() => handleLeaderboardToggle('games')}
+          >
+            Games
+          </button>
+          <button
+            className={leaderboardOption === 'stats' ? 'active' : ''}
+            onClick={() => handleLeaderboardToggle('stats')}
+          >
+            Stats
+          </button>
+        </div>
+      </div>
+
+      {leaderboardOption === 'games' && (
+        <div>
           <div className="leaderboard-table">
-            <div className="flex">
-              <h3>{username}</h3>
-              <div className="item">{(JSON.parse(player_info[0].stats).net >= 0 ? "+$" : "-$") + Math.abs(JSON.parse(player_info[0].stats).net)}</div>
-            </div>
-            {player_info.map(game => (
+            {currentGames.map((game) => (
               <Link to={`/game/${game.id}`} key={game.id} className="leaderboard-entry">
                 <div className="flex">
-                <div className="item">
-                  {game.name.replace("logs/", "")}
-                </div>
-                <div className="item">
-                  {new Date(game.date).toLocaleDateString()}
-                </div>
+                  <div className="item">{game.name.replace('logs/', '')}</div>
+                  <div className="item">{new Date(game.date).toLocaleDateString()}</div>
                 </div>
               </Link>
             ))}
           </div>
 
-          <table className="leaderboard-table">
-            <tbody>
-                {statistics_entries.map(([name, data]) => (
-                <tr key={name}>
-                    <td>{name}</td>
-                    <td>{data[0]} / {data[1]}</td>
-                </tr>
-                ))}
-            </tbody>
-            </table>
+          <div className="pagination">
+            {player_info.length > gamesPerPage && (
+              <ul>
+                {Array(Math.ceil(player_info.length / gamesPerPage))
+                  .fill()
+                  .map((_, index) => (
+                    <li
+                      key={index}
+                      className={currentPage === index + 1 ? 'active' : ''}
+                      onClick={() => paginate(index + 1)}
+                    >
+                      {index + 1}
+                    </li>
+                  ))}
+              </ul>
+            )}
+          </div>
         </div>
-      );
+      )}
+
+      {leaderboardOption === 'stats' && (
+        <table className="stats-table">
+          <tbody>
+            {statistics_entries.map(([name, data]) => (
+              <tr key={name}>
+                <td>{name}</td>
+                <td>{data[0]} / {data[1]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
+
 
 export const playerInfoLoader = async ({ params }) => {
     const { username } = params
--- client/src/RootLayout.js
+++ client/src/RootLayout.js
@@ -1,13 +1,14 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 export default function RootLayout() {
     return (
         <div className="rootlayout">
-            <h1 className="title">DonkHouse Live Tracker</h1>
-
+            <Link to="/" className="home-link">
+                <h1 className="title">DonkHouse Live Tracker</h1>
+            </Link>
             <main>
                 <Outlet />
-            </main>
+            </main> 
         </div>
     )
 }
